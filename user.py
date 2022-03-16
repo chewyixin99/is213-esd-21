2d53897e3ad3dfbd0ac015e3e3303fc674be6254
hellobiondi <hellobiondi@protonmail.com>
2022-03-16 16:04:55 +0800

    added ":root"
    added ":root" to provide compatibility for MacOS

--- user.py
+++ user.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # Flask SQLAlchemy config
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://root@localhost:3306/esd"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://root:root@localhost:3306/esd"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
