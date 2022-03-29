df6c75069517684d72af7f8e6ee7626ffbd773af
joellim2020 <joel.lim.2020@scis.smu.edu.sg>
2022-03-30 02:41:19 +0800

    basic interface

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,15 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="nav">
+    <NavBar/>
+    <router-link to="/">Login</router-link> |
+    <router-link to="/Hawkers">About</router-link>
+
+    <router-view/>
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NavBar,
   }
 }
 </script>
@@ -21,6 +28,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
 </style>
---
+++ hello-world/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Login/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Login from '@/components/Login-comp.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Login
+  }
+}
+</script>
