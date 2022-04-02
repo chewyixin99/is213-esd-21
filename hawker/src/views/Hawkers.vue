45575738e0a5d0c80a03cb1045a30d607ec67e0c
joellim2020 <joel.lim.2020@scis.smu.edu.sg>
2022-04-02 19:32:23 +0800

    help debug pls

--- hawker/src/views/Hawkers.vue
+++ hawker/src/views/Hawkers.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id="app">
     <!-- Welcome Message and Wallet -->
     <div class="grid grid-cols-5 shadow bgimg">
       <div class="col-span-3 text-left p-3 my-auto">
@@ -25,17 +25,67 @@
 </template>
 
 <script>
-// @ is an alias to /src
-import Hawker from '@/components/Hawker-comp.vue'
-import Wallet from '@/components/Wallet-comp.vue'
+  // @ is an alias to /src
+  import Hawker from '@/components/Hawker-comp.vue'
+  import Wallet from '@/components/Wallet-comp.vue'
 
-export default {
-  name: 'Hawkers',
-  components: {
-    Hawker,
-    Wallet,
+  export default {
+    name: 'Hawkers',
+    components: {
+      Hawker,
+      Wallet,
+    }
   }
-}
+  console.log("Hello")
+
+  const get_hawker_URL = "http://localhost:5002/hawker";
+
+  const app = Vue.createApp({
+    computed: {
+      hasHawkers: function(){
+        console.log("Compute Hawker")
+        // return this.hawkers.length > 0;
+      }
+    },
+    data(){
+      return {
+        hawkers: [],
+        // closing_hours: "", 
+        // cuisine: "", 
+        // email: "", 
+        // halal: false, 
+        // has_vegetarian_option: false, 
+        // hawker_id: "", 
+        // opening_hours: "", 
+        // password: "", 
+        // username: "", 
+        // wallet_id: null
+      };
+    },
+    methods: {
+      getAllHawkers(){
+        console.log("=== Open Hawker ===")
+        const response =
+          fetch(get_hawker_URL)
+            .then( data => {
+              console.log(response);
+              if (data.code === 404){
+                this.message = data.message;
+              } else {
+                this.hawkers = data.data.hawkers;
+              }
+            })
+            .catch(error => {
+              console.log(this.message + error);
+            });
+      },
+
+    }
+    
+
+  });
+  app.mount('#app');
+
 </script>
 
 
