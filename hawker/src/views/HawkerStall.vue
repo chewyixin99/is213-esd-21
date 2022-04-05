59a59b08303871e32335a96e1a4667988821089f
Kok Wee LOH <lohkokwee@gmail.com>
2022-04-05 18:51:09 +0800

    logic for hawker>hawkerstall>item selection scenario

--- hawker/src/views/HawkerStall.vue
+++ hawker/src/views/HawkerStall.vue
@@ -2,33 +2,59 @@
   <div>
     <!-- Welcome Message and Wallet -->
     <div class="bg-warning text-gray text-lg font-medium py-3">
-      <span>Hawker Name</span>
+      <i>Hawker Stall</i>
+      <p class="text-3xl">{{ hawker_name }}</p>
     </div>
 
-    <div class="mt-3">
-      <Item/>
-      <Item/>
-      <Item/>
+    <div v-if="items === null" class="mt-3">
+      <p>No items.</p>
     </div>
-
-
+    <div v-else class="mt-3">
+      <Item v-for="item in items" :key="item.item_id" :item_data_prop="item"/>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Item from '@/components/Item-comp.vue'
-
+import axios from "axios";
+import Item from "@/components/Item-comp.vue";
 
 export default {
-  name: 'HawkerStall',
+  name: "HawkerStall",
   components: {
     Item,
-  }
-}
+  },
+
+  data() {
+    return {
+      hawker_id: null,
+      hawker_name: null,
+      items: null,
+    };
+  },
+
+  created: function () {
+    this.hawker_id = this.$route.query.hawker_id;
+    this.hawker_name = this.$route.query.hawker_name;
+
+    console.log(`=== getting ${this.hawker_id}'s items ===`);
+    const get_Item_URL = `http://localhost:5003/item/hawker/${this.hawker_id}`;
+
+    axios
+      .get(get_Item_URL)
+      .then((response) => {
+        console.log(response.data.data.items);
+        this.items = response.data.data.items;
+      })
+      .catch((error) => {
+        console.log(`=== ERROR getting ${this.hawker_id}'s items ===`);
+        console.log(error.message);
+      });
+  },
+};
 </script>
 
 
 <style scoped>
-
 </style>
